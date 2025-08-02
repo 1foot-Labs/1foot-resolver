05b59099523cb568d53d573a28b5ad3c9a18d561
raehat <raehatsinghnanda@gmail.com>
2025-08-03 02:00:40 +0530

    change timelock in Ethereum HTLC

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"encoding/json"
 	"net/http"
 	"sort"
+    "time"
 
 	"fmt"
 
 	"os/exec"
 
-	"math/big"
-	"resolver/HTLC"
+    "math/big"
 	"resolver/models"
+    "resolver/HTLC"
+    "resolver/relayer_communication"
 )
 
 func lookForActiveOrders() {
@@ -43,15 +45,15 @@ func lookForActiveOrders() {
 	fmt.Println("✅ Latest Order:")
 	fmt.Printf("ID: %s | ETH Addr: %s | sha256: %s\n", latest.ID, latest.MakerAddress, latest.Sha256)
 
-	HTLC.CreateHTLCForBitcoin(latest.Sha256, latest.PubKey)
+	_, p2shAddress := HTLC.CreateHTLCForBitcoin(latest.Sha256, latest.PubKey)
 
-	clientURL := "http://localhost:8545"                                                  // your Ethereum node
-	privateKeyHex := "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" // your private key (without 0x prefix or with)
+    clientURL := "http://localhost:8545" // your Ethereum node
+	privateKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"  // resolver pvt key
 	contractAddressHex := "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-	receiverHex := latest.MakerAddress                       // receiver's Ethereum address
-	secretHex := latest.Sha256                               // hex-encoded secret (32 bytes)
-	timelockSeconds := int64(3600 * 24)                      // 24 hour
-	amountInWei := big.NewInt(int64(latest.AmountToReceive)) // 1 ETH in wei
+	receiverHex := latest.MakerAddress 
+	secretHex := latest.Sha256       
+	timelockSeconds := int64(3600 * 24)  
+	amountInWei := big.NewInt(int64(latest.AmountToReceive))     
 
 	// Call the function
 	txHash, err := HTLC.CreateHTLCForEthereum(
@@ -69,6 +71,22 @@ func lookForActiveOrders() {
 	}
 
 	fmt.Println("HTLC created with tx hash:", txHash)
+
+    request := relayer_communication.FulfillOrderRequest{
+		OrderID:        latest.ID,
+		TakerAddress:   "",
+		EthHTLCAddress: contractAddressHex,
+		BtcHTLCAddress: p2shAddress,
+	}
+
+	resp, err = relayer_communication.FulfillOrder(request)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Status Code:", resp.StatusCode)
 }
 
 func getCurrentBlockHeight() (int64, error) {
@@ -87,39 +105,39 @@ func getCurrentBlockHeight() (int64, error) {
 }
 
 func main() {
-	// ticker := time.NewTicker(1 * time.Minute)
-	// defer ticker.Stop()
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
 
-	// lookForActiveOrders()
+	lookForActiveOrders()
 
-	// for range ticker.C {
-	// 	lookForActiveOrders()
-	// }
+	for range ticker.C {
+		lookForActiveOrders()
+	}
 	// HTLC.CreateHTLCForBitcoin("6b6a5987b7a4cbbf2310cb1e785df165f83a6e44",
 	// 	"022514f3c0d22eac4d45ecc6ed9fb17fa44cebb88d590b79ca834b20a552f9bb67")
 
-	clientURL := "http://localhost:8545"                                                // your Ethereum node
-	privateKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" // your private key (without 0x prefix or with)
-	contractAddressHex := "0x0165878A594ca255338adfa4d48449f69242Eb8P"
-	receiverHex := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"                     // receiver's Ethereum address
-	secretHex := "fc56dbc6d4652b315b86b71c8d688c1ccdea9c5f1fd07763d2659fde2e2fc49a" // hex-encoded secret (32 bytes)
-	timelockSeconds := int64(3600 * 24)                                             // 24 hour
-	amountInWei := big.NewInt(1e18)                                                 // 1 ETH in wei
+    // clientURL := "http://127.0.0.1:8545" // your Ethereum node
+	// privateKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"       // your private key (without 0x prefix or with)
+	// contractAddressHex := "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
+	// receiverHex := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" // receiver's Ethereum address
+	// secretSHA256Hex := "fc56dbc6d4652b315b86b71c8d688c1ccdea9c5f1fd07763d2659fde2e2fc49a"       // hex-encoded secret (32 bytes)
+	// timelockSeconds := int64(3600 * 24)       // 24 hour
+	// amountInWei := big.NewInt(1e18)      // 1 ETH in wei
 
-	// Call the function
-	txHash, err := HTLC.CreateHTLCForEthereum(
-		clientURL,
-		privateKeyHex,
-		contractAddressHex,
-		receiverHex,
-		secretHex,
-		timelockSeconds,
-		amountInWei,
-	)
-	if err != nil {
-		fmt.Println("Error creating HTLC:", err)
-		return
-	}
+	// // Call the function
+	// txHash, err := HTLC.CreateHTLCForEthereum(
+	// 	clientURL,
+	// 	privateKeyHex,
+	// 	contractAddressHex,
+	// 	receiverHex,
+	// 	secretSHA256Hex,
+	// 	timelockSeconds,
+	// 	amountInWei,
+	// )
+	// if err != nil {
+	// 	fmt.Println("Error creating HTLC:", err)
+	// 	return
+	// }
 
-	fmt.Println("HTLC created with tx hash:", txHash)
+	// fmt.Println("HTLC created with tx hash:", txHash)
 }
